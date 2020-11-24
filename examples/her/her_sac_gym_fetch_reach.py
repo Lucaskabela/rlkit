94a7179cb3a679c5b9ebbb4b87520dd6e20f4f54
Lucaskabela <lucaskabela@utexas.edu>
2020-11-23 20:04:08 -0600

    MountainCar stuff

--- examples/her/her_sac_gym_fetch_reach.py
+++ examples/her/her_sac_gym_fetch_reach.py
@@ -9,134 +9,190 @@ from rlkit.torch.networks import ConcatMlp
 from rlkit.torch.sac.policies import MakeDeterministic, TanhGaussianPolicy
 from rlkit.torch.sac.sac import SACTrainer
 from rlkit.torch.torch_rl_algorithm import TorchBatchRLAlgorithm
-from robosuite.wrappers import Wrapper
-import robosuite as suite
+# from robosuite.wrappers import Wrapper
+# import robosuite as suite
 import numpy as np
 
-class DoorWrapper(Wrapper):
-    """
-    Initializes the Gym wrapper. Mimics many of the required functionalities of the Wrapper class
-    found in the gym.core module
-    Args:
-        env (MujocoEnv): The environment to wrap.
-        keys (None or list of str): If provided, each observation will
-            consist of concatenated keys from the wrapped environment's
-            observation dictionary. Defaults to robot-state and object-state.
-    Raises:
-        AssertionError: [Object observations must be enabled if no keys]
-    """
 
-    def __init__(self, env, keys=None):
-        # Run super method
-        super().__init__(env=env)
-        # Create name for gym
-        robots = "".join([type(robot.robot_model).__name__ for robot in self.env.robots])
-        self.name = robots + "_" + type(self.env).__name__
+class GoalMountainCar(gym.Wrapper):
 
-        # Get reward range
-        self.reward_range = (0, self.env.reward_scale)
-
-        if keys is None:
-            assert self.env.use_object_obs, "Object observations need to be enabled."
-            keys = ["object-state"]
-            # Iterate over all robots to add to state
-            for idx in range(len(self.env.robots)):
-                keys += ["robot{}_robot-state".format(idx)]
-        self.keys = keys
-
-        # Gym specific attributes
-        self.env.spec = None
-        self.metadata = None
-        self.goal = np.array([.3])
-
-        # set up observation and action spaces
-        flat_ob = self._flatten_obs(self.env.reset(), verbose=True)
-        self.obs_dim = flat_ob.size
-        high = np.inf * np.ones(self.obs_dim)
-        low = -high
-        self.observation_space = gym.spaces.Dict({"observation": gym.spaces.Box(low=low, high=high), "achieved_goal": gym.spaces.Box(low=np.zeros(1), high=np.ones(1), shape=(1,)), "desired_goal": gym.spaces.Box(low=np.zeros(1), high=np.ones(1), shape=(1,))})
-        low, high = self.env.action_spec
-        self.action_space = gym.spaces.Box(low=low, high=high)
-
-    def _flatten_obs(self, obs_dict, verbose=False):
-        """
-        Filters keys of interest out and concatenate the information.
-        Args:
-            obs_dict (OrderedDict): ordered dictionary of observations
-            verbose (bool): Whether to print out to console as observation keys are processed
-        Returns:
-            np.array: observations flattened into a 1d array
-        """
-        ob_lst = []
-        for key in obs_dict:
-            if key in self.keys:
-                if verbose:
-                    print("adding key: {}".format(key))
-                ob_lst.append(obs_dict[key])
-        return np.concatenate(ob_lst)
-
-    def reset(self):
-        """
-        Extends env reset method to return flattened observation instead of normal OrderedDict.
-        Returns:
-            np.array: Flattened environment observation space after reset occurs
-        """
-        ob_dict = self.env.reset()
-        state = self._flatten_obs(ob_dict)
-        ag = np.array(self.env.sim.data.qpos[self.env.hinge_qpos_addr])
-        g = self.goal
-        return {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
-
-    def step(self, action):
-        """
-        Extends vanilla step() function call to return flattened observation instead of normal OrderedDict.
-        Args:
-            action (np.array): Action to take in environment
-        Returns:
-            4-tuple:
-                - (np.array) flattened observations from the environment
-                - (float) reward from the environment
-                - (bool) whether the current episode is completed or not
-                - (dict) misc information
-        """
-        ob_dict, reward, done, info = self.env.step(action)
-        state = self._flatten_obs(ob_dict)
-        ag = np.array(self.env.sim.data.qpos[self.env.hinge_qpos_addr])
-        g = self.goal
-        ob_dict = {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
-        return ob_dict, reward, done, info
-
-    def seed(self, seed=None):
-        """
-        Utility function to set numpy seed
-        Args:
-            seed (None or int): If specified, numpy seed to set
-        Raises:
-            TypeError: [Seed must be integer]
-        """
-        # Seed the generator
-        if seed is not None:
-            try:
-                np.random.seed(seed)
-            except:
-                TypeError("Seed must be an integer type!")
+    def reset(self, **kwargs):
+        state = self.env.reset(**kwargs)
+        ag = np.array(self.env.state)
+        g = np.array([self.env.goal_position, self.env.goal_velocity])
+        state = {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
+        return state
 
     def compute_reward(self, achieved_goal, desired_goal, info):
-        return 1 if achieved_goal[0] > desired_goal[0] else 0
+        shape = False
+        dense = 100*((math.sin(3*achieved_goal[0]) * 0.0025 + 0.5 * achieved_goal[1] * achieved_goal[1]) - (math.sin(3*desired_goal[0]) * 0.0025 + 0.5 * desired_goal[1] * desired_goal[1])) 
+        if achieved_goal[0] != desired_goal[0]:
+            return -1 if not shape else dense
+        else:
+            return 0 if achieved_goal[0] >= desired_goal[0] else (-1 if not shape else dense)
 
-def make_env():
-    env = DoorWrapper(
-            suite.make(
-                "Door",
-                robots="Sawyer",                # use Sawyer robot
-                use_camera_obs=False,           # do not use pixel observations
-                has_offscreen_renderer=False,   # not needed since not using pixel obs
-                has_renderer=False,              # make sure we can render to the screen
-                reward_shaping=False,            # use dense rewards
-                control_freq=20,                # control should happen fast enough so that simulation looks smooth
-            )
-        )
-    return env
+    def step(self, action):
+        state, _, done, info = super().step(action)
+        ag = np.array(self.env.state)
+        g = np.array([self.env.goal_position, self.env.goal_velocity])
+        reward = self.compute_reward(ag, g, info)
+        state = {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
+        info['is_success'] = reward==0
+        return state, reward, done, info
+
+class GoalMountainCarContinuous(gym.Wrapper):
+
+    def reset(self, **kwargs):
+        state = self.env.reset(**kwargs)
+        ag = np.array(self.env.state)
+        g = np.array([self.env.goal_position, self.env.goal_velocity])
+        state = {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
+        return state
+
+    def compute_reward(self, achieved_goal, desired_goal, info):
+        shape = False
+        dense = 100*((math.sin(3*achieved_goal[0]) * 0.0025 + 0.5 * achieved_goal[1] * achieved_goal[1]) - (math.sin(3*desired_goal[0]) * 0.0025 + 0.5 * desired_goal[1] * desired_goal[1])) 
+        if achieved_goal[0] != desired_goal[0]:
+            return -1 if not shape else dense
+        else:
+            return 0 if achieved_goal[0] >= desired_goal[0] else (-1 if not shape else dense)
+
+
+    def step(self, action):
+        state, _, done, info = super().step(action)
+        ag = np.array(self.env.state)
+        g = np.array([self.env.goal_position, self.env.goal_velocity])
+        reward = self.compute_reward(ag, g, None)
+        state = {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
+        info['is_success'] = ag[0] == g[0]
+        return state, reward, done, info
+
+# class DoorWrapper(Wrapper):
+#     """
+#     Initializes the Gym wrapper. Mimics many of the required functionalities of the Wrapper class
+#     found in the gym.core module
+#     Args:
+#         env (MujocoEnv): The environment to wrap.
+#         keys (None or list of str): If provided, each observation will
+#             consist of concatenated keys from the wrapped environment's
+#             observation dictionary. Defaults to robot-state and object-state.
+#     Raises:
+#         AssertionError: [Object observations must be enabled if no keys]
+#     """
+
+#     def __init__(self, env, keys=None):
+#         # Run super method
+#         super().__init__(env=env)
+#         # Create name for gym
+#         robots = "".join([type(robot.robot_model).__name__ for robot in self.env.robots])
+#         self.name = robots + "_" + type(self.env).__name__
+
+#         # Get reward range
+#         self.reward_range = (0, self.env.reward_scale)
+
+#         if keys is None:
+#             assert self.env.use_object_obs, "Object observations need to be enabled."
+#             keys = ["object-state"]
+#             # Iterate over all robots to add to state
+#             for idx in range(len(self.env.robots)):
+#                 keys += ["robot{}_robot-state".format(idx)]
+#         self.keys = keys
+
+#         # Gym specific attributes
+#         self.env.spec = None
+#         self.metadata = None
+#         self.goal = np.array([.3])
+
+#         # set up observation and action spaces
+#         flat_ob = self._flatten_obs(self.env.reset(), verbose=True)
+#         self.obs_dim = flat_ob.size
+#         high = np.inf * np.ones(self.obs_dim)
+#         low = -high
+#         self.observation_space = gym.spaces.Dict({"observation": gym.spaces.Box(low=low, high=high), "achieved_goal": gym.spaces.Box(low=np.zeros(1), high=np.ones(1), shape=(1,)), "desired_goal": gym.spaces.Box(low=np.zeros(1), high=np.ones(1), shape=(1,))})
+#         low, high = self.env.action_spec
+#         self.action_space = gym.spaces.Box(low=low, high=high)
+
+#     def _flatten_obs(self, obs_dict, verbose=False):
+#         """
+#         Filters keys of interest out and concatenate the information.
+#         Args:
+#             obs_dict (OrderedDict): ordered dictionary of observations
+#             verbose (bool): Whether to print out to console as observation keys are processed
+#         Returns:
+#             np.array: observations flattened into a 1d array
+#         """
+#         ob_lst = []
+#         for key in obs_dict:
+#             if key in self.keys:
+#                 if verbose:
+#                     print("adding key: {}".format(key))
+#                 ob_lst.append(obs_dict[key])
+#         return np.concatenate(ob_lst)
+
+#     def reset(self):
+#         """
+#         Extends env reset method to return flattened observation instead of normal OrderedDict.
+#         Returns:
+#             np.array: Flattened environment observation space after reset occurs
+#         """
+#         ob_dict = self.env.reset()
+#         state = self._flatten_obs(ob_dict)
+#         ag = np.array(self.env.sim.data.qpos[self.env.hinge_qpos_addr])
+#         g = self.goal
+#         return {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
+
+#     def step(self, action):
+#         """
+#         Extends vanilla step() function call to return flattened observation instead of normal OrderedDict.
+#         Args:
+#             action (np.array): Action to take in environment
+#         Returns:
+#             4-tuple:
+#                 - (np.array) flattened observations from the environment
+#                 - (float) reward from the environment
+#                 - (bool) whether the current episode is completed or not
+#                 - (dict) misc information
+#         """
+#         ob_dict, reward, done, info = self.env.step(action)
+#         state = self._flatten_obs(ob_dict)
+#         ag = np.array(self.env.sim.data.qpos[self.env.hinge_qpos_addr])
+#         g = self.goal
+#         ob_dict = {'observation': state, 'achieved_goal': ag, 'desired_goal': g}
+#         return ob_dict, reward, done, info
+
+#     def seed(self, seed=None):
+#         """
+#         Utility function to set numpy seed
+#         Args:
+#             seed (None or int): If specified, numpy seed to set
+#         Raises:
+#             TypeError: [Seed must be integer]
+#         """
+#         # Seed the generator
+#         if seed is not None:
+#             try:
+#                 np.random.seed(seed)
+#             except:
+#                 TypeError("Seed must be an integer type!")
+
+#     def compute_reward(self, achieved_goal, desired_goal, info):
+#         return 1 if achieved_goal[0] > desired_goal[0] else 0
+
+# def make_env():
+#     env = DoorWrapper(
+#             suite.make(
+#                 "Door",
+#                 robots="Sawyer",                # use Sawyer robot
+#                 use_camera_obs=False,           # do not use pixel observations
+#                 has_offscreen_renderer=False,   # not needed since not using pixel obs
+#                 has_renderer=False,              # make sure we can render to the screen
+#                 reward_shaping=False,            # use dense rewards
+#                 control_freq=20,                # control should happen fast enough so that simulation looks smooth
+#             )
+#         )
+#     return env
+# GoalMountainCarContinuous(gym.make("MountainCarContinuous-v0"))
+# GoalMountainCar(gym.make(MountainCar-v0))
 
 def experiment(variant):
     # unwrap the TimeLimitEnv wrapper since we manually termiante after 50 steps
